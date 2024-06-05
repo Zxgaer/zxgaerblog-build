c6cc481777499bc170b6273aa87d81a6f2a94e5f
Zxgaer <zaixiage@outlook.com>
2024-06-05 22:07:44 +0800

    await

---
+++ assets/friendship-link.html-a027adeb.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./plugin-vue_export-helper-c27b6911.js";import{o as r,c as n}from"./app-b77eb5d6.js";const t={};function c(o,i){return r(),n("div")}const f=e(t,[["render",c],["__file","friendship-link.html.vue"]]);export{f as default};
